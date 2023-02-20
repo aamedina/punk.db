586fb5a8cdb59a15fa138d08015eea9988a63451
Adrian Medina <adrian@wikipunk.net>
2023-02-20 10:24:39 -0500

    mop/find-class-using-env

--- src/net/wikipunk/datomic.clj
+++ src/net/wikipunk/datomic.clj
@@ -5,8 +5,11 @@
   :conn -- datomic client connection
   :db-name -- name of the database"
   (:require
+   [clojure.walk :as walk]
    [com.stuartsierra.component :as com]
-   [datomic.client.api :as d]))
+   [datomic.client.api :as d]
+   [net.wikipunk.mop :as mop]
+   [net.wikipunk.rdf :as rdf]))
 
 (defrecord Connection [client db-name]
   com/Lifecycle
@@ -15,3 +18,14 @@
     (assoc this :conn (d/connect client this)))
   (stop [this]
     (assoc this :conn nil)))
+
+(defmethod mop/find-class-using-env [clojure.lang.Keyword datomic.core.db.Db]
+  [ident env]
+  (dissoc (walk/postwalk (fn [form]
+                           (if (and (map? form)
+                                    (contains? form :db/ident)
+                                    (not= ident (:db/ident form)))
+                             (:db/ident form)
+                             form))
+                         (d/pull env '[*] ident))
+          :db/id))
