fc296e4510171742eb834c1857a520cabf9a5ad2
Adrian Medina <adrian@wikipunk.net>
2023-02-20 11:07:48 -0500

    implement datomic.client.api.protocols on Connection component

--- src/net/wikipunk/datomic.clj
+++ src/net/wikipunk/datomic.clj
@@ -8,16 +8,63 @@
    [clojure.walk :as walk]
    [com.stuartsierra.component :as com]
    [datomic.client.api :as d]
+   [datomic.client.api.protocols :as impl]
    [net.wikipunk.mop :as mop]
    [net.wikipunk.rdf :as rdf]))
 
-(defrecord Connection [client db-name]
+(defrecord Connection [client db-name conn]
   com/Lifecycle
   (start [this]
     (d/create-database client this)
     (assoc this :conn (d/connect client this)))
   (stop [this]
-    (assoc this :conn nil)))
+    (assoc this :conn nil))
+
+  impl/Db
+  (as-of [_ time-point]
+    (impl/as-of (d/db conn) time-point))
+  (datoms [_ arg-map]
+    (impl/datoms (d/db conn) arg-map))
+  (db-stats [_]
+    (impl/db-stats (d/db conn)))
+  (history [_]
+    (impl/history (d/db conn)))
+  (index-pull [_ arg-map]
+    (impl/index-pull (d/db conn) arg-map))
+  (index-range [_ arg-map]
+    (impl/index-range (d/db conn) arg-map))
+  (pull [_ arg-map]
+    (impl/pull (d/db conn) arg-map))
+  (pull [_ selector eid]
+    (impl/pull (d/db conn) selector eid))
+  (since [_ t]
+    (impl/since (d/db conn) t))
+  (with [_ arg-map]
+    (impl/with (d/db conn) arg-map))
+
+  impl/Connection
+  (db [_]
+    (d/db conn))
+  (transact [_ arg-map]
+    (impl/transact conn arg-map))
+  (sync [_ t]
+    (impl/sync conn t))
+  (tx-range [_ arg-map]
+    (impl/tx-range conn arg-map))
+  (with-db [_]
+    (impl/with-db conn))
+
+  impl/Client
+  (administer-system [_ arg-map]
+    (impl/administer-system client arg-map))
+  (list-databases [_ arg-map]
+    (impl/list-databases client arg-map))
+  (connect [_ arg-map]
+    (impl/connect client arg-map))
+  (create-database [_ arg-map]
+    (impl/create-database client arg-map))
+  (delete-database [_ arg-map]
+    (impl/delete-database client arg-map)))
 
 (defmethod mop/find-class-using-env [clojure.lang.Keyword datomic.core.db.Db]
   [ident env]
