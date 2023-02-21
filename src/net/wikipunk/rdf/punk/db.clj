bf777eca17c6561194e51dbcc750f2968fcaa311
Adrian Medina <adrian@wikipunk.net>
2023-02-21 16:23:28 -0500

    :punk.db/Index class

--- src/net/wikipunk/rdf/punk/db.clj
+++ src/net/wikipunk/rdf/punk/db.clj
@@ -1,4 +1,21 @@
 (ns net.wikipunk.rdf.punk.db
   {:rdf/type :owl/Ontology})
 
-;; Begin modelling your future hyperreality today!
+(def Index
+  {:db/ident :punk.db/Index
+   :rdf/type :owl/Class})
+
+(def SpatialIndex
+  {:db/ident        :punk.db/SpatialIndex
+   :rdf/type        :owl/Class
+   :rdfs/subClassOf :punk.db/Index})
+
+(def TemporalIndex
+  {:db/ident        :punk.db/TemporalIndex
+   :rdf/type        :owl/Class
+   :rdfs/subClassOf :punk.db/Index})
+
+(def FullTextIndex
+  {:db/ident        :punk.db/FullTextIndex
+   :rdf/type        :owl/Class
+   :rdfs/subClassOf :punk.db/Index})
